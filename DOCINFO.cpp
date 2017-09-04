e9368b3b27a5ec3015e7a6af29ed79df1174f0d6
steve-willbit <31562124+steve-willbit@users.noreply.github.com>
2017-09-04 05:05:06 +0200

    DocInfo.cpp
    Import legacy code

---
+++ DOCINFO.cpp
@@ -0,0 +1,463 @@
+
+/* ----------------------------------------------------------------------------------- *
+
+	DocInfo.cpp
+
+	11 Aprile 2005						Fabrizio
+
+	Descrizione:
+	Contiene delle procedure per prelevare informazioni sul box corrente
+	
+	$Log: not supported by cvs2svn $
+	Revision 1.1  2005/04/20 07:14:18  taretto
+	importazione files
+	
+
+* ----------------------------------------------------------------------------------- */
+
+// CONFIG INCLUDES
+// CONFIG INCLUDES
+// CONFIG INCLUDES
+
+// always the first
+#include "XTConfig.h"
+#include "QXPConfig.h"
+
+// STANDARD INCLUDES
+// STANDARD INCLUDES
+// STANDARD INCLUDES
+
+#if QXP60
+#if defined(__MWERKS__) && defined(__MACH__)
+	#define TARGET_API_MAC_OSX 1
+	#include <MSL MacHeadersMach-O.h>
+#endif // defined(__MWERKS__) && defined(__MACH__)
+#endif // QXP60
+
+#include <cassert>
+
+// DBP INCLUDES
+// DBP INCLUDES
+// DBP INCLUDES
+
+#include "DbpInclude.h"
+
+// PROJECT INCLUDES
+// PROJECT INCLUDES
+// PROJECT INCLUDES
+
+#include "Coord.h"
+#include "Errori.h"
+#include "Pref.h"
+#include "XTNecro.h"
+
+#include "DocInfo.h"
+
+// CONSTS
+// CONSTS
+// CONSTS
+
+#define kPrimoSpread 1
+#define kZeroCinquePunti 0x00008000
+
+// STATICS GLOBALS
+// STATICS GLOBALS
+// STATICS GLOBALS
+
+// record che contiene le informazioni sulla pagina
+static newpageinfo *gPtrInfoPagina = NULL;
+
+static Fixed gAltezzaPagina;
+static Fixed gLarghezzaPagina;
+static bool8 gDoubleSided;
+static DocID gCurDoc;
+
+// FUNCTIONS
+// FUNCTIONS
+// FUNCTIONS
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiDocInfo
+
+* ------------------------------------------------------------------------ */
+errorixtension XTAPI PrendiDocInfo() throw()
+{
+	// identificatore del boxid dello spread
+	spreadboxid  lIdBoxSpread = 0;
+	// punatore allo spread 
+	xtboxptr lPtrBoxSpread = NULL;
+	// handle ai dati delle pagine
+	pdatahandle lHndlDatiPagina = NULL;
+	// doc corrente
+	DocID gCurDoc;
+	xtget_curdoc(&gCurDoc);
+	
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		// annullo le info precendenti
+		gAltezzaPagina = 0;
+		gLarghezzaPagina = 0;
+		gDoubleSided = 0;
+		
+		// (ri)prendo le info
+		DocRef DRef = XTGetDocRefFromDocID(gCurDoc);
+    		XTGetDocPageLength(DRef, &gAltezzaPagina);
+    		XTGetDocPageWidth(DRef, &gLarghezzaPagina);
+		XTGetDoubleSided(DRef, &gDoubleSided);
+		
+/*		<-- la struttura docsysrec non e' piu' supportata
+		if (gPtrDocInfoRec != NULL) 
+		{
+			DisposPtr((Ptr) gPtrDocInfoRec);
+			gPtrDocInfoRec = NULL;
+		}
+*/		
+		if (gPtrInfoPagina != NULL) 
+		{
+			// DisposePtr((Ptr) gPtrInfoPagina);
+			gPtrInfoPagina = NULL;
+		}
+
+/*		<-- la struttura docsysrec non e' piu' supportata		
+		// creo il puntatore per le info del documento
+		gPtrDocInfoRec = (docsysrec *) NewPtrClear(sizeof(docsysrec));
+		if (gPtrDocInfoRec == NULL) 
+		{
+			// errore nell'allocazione della memoria
+			return(kErroreMemoria);
+		}
+		
+		// prendo le info del documento
+		getdocinfo(gPtrDocInfoRec);
+		
+		// creo il puntatore per le info della pagina 
+		gPtrInfoPagina = (newpageinfo *) NewPtrClear(sizeof(newpageinfo));
+		if (gPtrInfoPagina == NULL) {
+			// errore nell'allocazione della memoria 
+			return(kErroreMemoria);
+		}
+*/		
+		// prendo spread box id
+		XTGetSpreadBox(DRef, kPrimoSpread, &lIdBoxSpread);
+		
+		// prendo puntatore al box
+		lPtrBoxSpread = getxtbox(NULL, lIdBoxSpread, FALSE);  
+		lHndlDatiPagina = lPtrBoxSpread->g.pagestuff;
+		&(*gPtrInfoPagina) = &(**lHndlDatiPagina).pgs[0];		
+		
+		// dispose del puntatore all spread box
+		disposextbox(lPtrBoxSpread,TRUE);  
+		
+	} else 
+	{
+		return(kErroreDelCodice);
+	}
+	
+	return(kNessunErrore);
+} // PrendiDocInfo
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiMargineAlto
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiMargineAlto() throw()
+{
+	// if (gPtrDocInfoRec != NULL) 
+	if (gCurDoc >= 0)
+	{
+		 // sono sicuro che e' aperto un documento
+		return((*gPtrInfoPagina).top - kZeroCinquePunti + gXtPreferences.documentoPref.altezzaTestata);
+	}
+	 else 
+	 {
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiMargineAlto
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiMargineBasso
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiMargineBasso() throw()
+{
+	Fixed lAltezzaPagina = 0;
+	Fixed lMargineBasso = 0;
+	Fixed lFondoColonna = 0;
+	
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		lMargineBasso = (*gPtrInfoPagina).bottom - kZeroCinquePunti;
+		lFondoColonna = gAltezzaPagina - lMargineBasso;
+		return(lFondoColonna);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiMargineBasso
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiMargineSinistro
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiMargineSinistro() throw()
+{
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		return((*gPtrInfoPagina).left - kZeroCinquePunti);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiMargineSinistro
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiMargineDestro
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiMargineDestro() throw()
+{
+	Fixed lMargineDestro = 0;
+	
+	if (gCurDoc >= 0)  
+	{
+		// sono sicuro che e' aperto un documento
+		lMargineDestro = gLarghezzaPagina - (*gPtrInfoPagina).right - kZeroCinquePunti;
+		return(lMargineDestro);
+	} 
+	else 
+	{
+		//non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiMargineDestro
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiLarghezzaColonna
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiLarghezzaColonna() throw()
+{
+	int16 lNumeroColonne = 0;
+	Fixed lLarghezzaPagina = 0;
+	Fixed lMargineSinistro = 0;
+	Fixed lMargineDestro = 0;
+	Fixed lDistanzaTraColonne = 0;
+	Fixed lLarghezzaColonna = 0;
+	
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento 		
+		lNumeroColonne = (*gPtrInfoPagina).colcount;
+		lLarghezzaPagina = gLarghezzaPagina;
+		lMargineSinistro = (*gPtrInfoPagina).left - kZeroCinquePunti;
+		lMargineDestro = (*gPtrInfoPagina).right - kZeroCinquePunti;
+		lDistanzaTraColonne = (*gPtrInfoPagina).gutter;
+		lLarghezzaColonna = lLarghezzaPagina - lMargineSinistro - lMargineDestro;
+		lLarghezzaColonna -= (lDistanzaTraColonne * (long) (lNumeroColonne - 1));
+
+		// variabile d'utilita
+		uchar tmpstr[kDimensioneStringhe] = "";
+		wsprintf((char*)tmpstr, "Errore di divisione per zero.\nSorgente: %s, Linea: %d.",__FILE__, __LINE__);
+
+		return(lLarghezzaColonna / (long) lNumeroColonne);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiLarghezzaColonna
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiAltezzaColonna
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiAltezzaColonna() throw()
+{
+	Fixed lAltezzaPagina = 0;
+	Fixed lMargineAlto = 0;
+	Fixed lMargineBasso = 0;
+	
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		lAltezzaPagina = gAltezzaPagina;
+		lMargineAlto = (*gPtrInfoPagina).top - kZeroCinquePunti;
+		lMargineBasso = (*gPtrInfoPagina).bottom - kZeroCinquePunti;
+		return(lAltezzaPagina - lMargineAlto - lMargineBasso - gXtPreferences.documentoPref.altezzaTestata);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiAltezzaColonna
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiNumeroColonne
+
+* ------------------------------------------------------------------------ */
+int16 XTAPI PrendiNumeroColonne() throw()
+{
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		return((*gPtrInfoPagina).colcount);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiNumeroColonne
+
+/* ------------------------------------------------------------------------ *
+
+	PrendiDistanzaColonne
+
+* ------------------------------------------------------------------------ */
+Fixed XTAPI PrendiDistanzaColonne() throw()
+{
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		return((*gPtrInfoPagina).gutter);
+	} 
+	else 
+	{
+		// non c'e' nessun documento aperto correntemente
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(0);
+	}
+} // PrendiDistanzaColonne
+
+/* ------------------------------------------------------------------------ *
+
+	IsPagineAffiancate
+
+* ------------------------------------------------------------------------ */
+bool8 XTAPI IsPagineAffiancate() throw()
+{
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		return(gDoubleSided);
+	} 
+	else 
+	{
+		// ritorno TRUE per bloccare l'impaginazione
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+		return(TRUE);
+	}
+} // IsPagineAffiancate
+
+/* ------------------------------------------------------------------------ *
+
+	ImpostaScalaDocumento
+
+* ------------------------------------------------------------------------ */
+void XTAPI ImpostaScalaDocumento(Fixed *percetuale) throw()
+{
+	Fixed docScale;
+	Fixed lPercentuale = 0;
+	
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che e' aperto un documento
+		lPercentuale = (*percetuale);
+		DocRef DRef = XTGetDocRefFromDocID(gCurDoc);
+		XTGetDocScale(DRef, &docScale);
+		(*percetuale) = docScale;
+		docScale = lPercentuale;
+		XTSetDocScale(DRef, docScale, 1, NULL);
+	} 
+	else 
+	{
+		// richiesta di impostare la scala del documento senza documento aperto
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+	}
+} // ImpostaScalaDocumento
+
+/* ------------------------------------------------------------------------ *
+
+	ImpostaFinestraDocumento
+
+* ------------------------------------------------------------------------ */
+void XTAPI ImpostaFinestraDocumento(int16 *larghezza, int16 *altezza) throw()
+{
+	int16       lAltezza = 0;
+	int16 	lLarghezza = 0;
+	Rect 	lRect;
+
+	if (gCurDoc >= 0) 
+	{
+		// sono sicuro che c'e' aperto un documento
+		GrafPtr	docWPtr = NULL;
+		xtget_docwptr(&docWPtr);
+		HWND docHWND = GetPortHWND(docWPtr);	
+		
+		GetWindowRect(docHWND, (RECT FAR *)&lRect );
+
+		lAltezza = (*altezza);
+		lLarghezza = (*larghezza);
+		(*larghezza) = lRect.right - lRect.left;
+		(*altezza) = lRect.bottom - lRect.top;
+		SetWindowPos(docHWND, HWND_TOP, 0,0,lLarghezza, lAltezza, SWP_NOMOVE);
+
+		// update della finestra
+		updatewnd((WindowPtr) docHWND);
+
+	} 
+	else 
+	{
+		// richiesta di impostare la dimensione della finestra del documento
+		// senza documento aperto
+		SysBeep(1);
+		SysBeep(1);
+		SysBeep(1);
+	}
+} // ImpostaFinestraDocumento
+
